389cc295f9b22d5bbbd8944c12bc24216db90df0
Kevin Smith <ksmitw@gmail.com>
2023-03-29 21:28:42 -0400

    Fixed my ThreadPoolExecutor so its implimented properly and added error handling.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import concurrent.futures
 import os
 import configparser
 import shutil
+import time
 
 
 def configure_paths():
@@ -23,7 +24,7 @@ def read_file_contents(file_content, counter):
         accounts.append(
             Account(file_content_array[0], file_content_array[1], file_content_array[2], file_content_array[3]))
         file_name = "account" + str(counter) + ".xml"
-    file_utilities.write_xml_file(accounts, counter, os.path.join(output_path, file_name))
+    file_utilities.write_xml_file(accounts,  os.path.join(output_path, file_name))
 
 
 if __name__ == '__main__':
@@ -35,13 +36,15 @@ if __name__ == '__main__':
             os.makedirs(output_path)
         except Exception as e:
             print(e)
+    else:
+        os.makedirs(output_path)
 
     os.chdir(input_path)
-    thread_pool = concurrent.futures.ThreadPoolExecutor(max_workers=5)
     counter = 0
-    for file in os.listdir():
-        if file.endswith('.txt'):
-            file_content = file_utilities.read_file_contents(file)
-            thread_pool.submit(read_file_contents(file_content, counter))
-            counter += 1
+    with concurrent.futures.ThreadPoolExecutor(30) as thread_pool:
+        for file in os.listdir():
+            if file.endswith('.txt'):
+                file_content = file_utilities.read_file_contents(file)
+                thread_pool.submit(read_file_contents, file_content, counter)
+                counter += 1
     thread_pool.shutdown(wait=True)
--- reader/file_utilities.py
+++ reader/file_utilities.py
@@ -1,36 +1,39 @@
 from xml.dom import minidom
 
-def write_xml_file(accounts, counter, output_path):
 
+def write_xml_file(accounts, output_path):
     doc = minidom.Document()
     root = doc.createElement('accounts')
     doc.appendChild(root)
 
     for user in accounts:
-        account = doc.createElement('account')
-        username = doc.createElement('user name')
+        try:
+            account = doc.createElement('account')
+            username = doc.createElement('user name')
 
-        username.appendChild(doc.createTextNode(user.name))
-        account.appendChild(username)
+            username.appendChild(doc.createTextNode(user.name))
+            account.appendChild(username)
 
-        account_id = doc.createElement('account id')
-        account_id.appendChild(doc.createTextNode(user.account_id))
-        account.appendChild(account_id)
+            account_id = doc.createElement('account id')
+            account_id.appendChild(doc.createTextNode(user.account_id))
+            account.appendChild(account_id)
 
-        address = doc.createElement('address')
-        address.appendChild(doc.createTextNode(user.address))
-        account.appendChild(address)
+            address = doc.createElement('address')
+            address.appendChild(doc.createTextNode(user.address))
+            account.appendChild(address)
 
-        phone_number = doc.createElement('phone number')
-        phone_number.appendChild(doc.createTextNode(user.phone_number))
-        account.appendChild(phone_number)
+            phone_number = doc.createElement('phone number')
+            phone_number.appendChild(doc.createTextNode(user.phone_number))
+            account.appendChild(phone_number)
 
-        root.appendChild(account)
+            root.appendChild(account)
+        except Exception as e:
+            print(e)
 
     doc.writexml(open(output_path, 'w'),
                  indent=" ",
                  addindent=" ",
-                 newl = "\n")
+                 newl="\n")
 
 
 def read_file_contents(input_file):
